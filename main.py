6407c03f2ba57142fd8d862551c8a6c8a8675ac4
Rootamiunik <142379304+Rootamiunik@users.noreply.github.com>
2023-08-23 22:24:43 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import os
-from dotenv import load_dotenv
-from auth import Auth
 from brain import Brain
-import ast
-import spotipy
 
 #--------------main-----------------#
-
-
 date = input("Playlist of Top 100 songs of date (YYYY-MM-DD):  ")
 
 brain = Brain(date)
